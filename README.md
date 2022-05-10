fa3f5a9c04644e0e6347d053f4473417043cc83c
AlexandroMarcelo <alex.fcomarcelo@gmail.com>
2022-05-10 00:00:10 -0500

    libraries added, cheatsheet

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Go Fundamentals
 
 [Cheatsheet](https://devhints.io/go)
+[Packages](https://awesome-go.com/)
---
+++ libraries/echo.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	// Init
+	e := echo.New()
+
+	// Route
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
